b69653bed1e93db82a1df6628446cd91a509588f
snehaldurgule <snehaldurgule1998@gmail.com>
2026-08-25 21:46:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "app" {
     ami = "ami-01a00762f46d584a1"  #regional specific
     instance_type = "t3.micro"
     availability_zone = "ap-south-1a" #optonal
+    associate_public_ip_address = false
     #key pair ,vpc ,firewall and storage ---optional
     tags = {
         name ="app_server"
@@ -15,8 +16,8 @@ resource "aws_instance" "web" {
     # arguments
     ami = "ami-0ac7b260cf76d8865"  #regional specific
     instance_type = "t3.micro"
+    associate_public_ip_address = false
 
-    
     tags = {
         name ="web_server"
         Env = "production" 
